7f34e2727bdd23ebea8eb980a9b30f19fbef1db2
sagae keiga <sagae5.28rujeae@gmail.com>
2017-02-16 07:09:07 +0000

    seijipress2

--- anemone-sqlite3.rb
+++ anemone-sqlite3.rb
@@ -16,3 +16,26 @@ Anemone.crawl(urls, opts) do |anemone|
 		puts page.doc.css('tbody').to_s
 	end
 end
+
+
+
+#　↓明日、調整します。
+
+
+# 文字コードをUTF8に設定
+connection = Mysql::connect("hostname", "username", "password", "dbname")
+
+# # 文字コードをUTF8に設定
+connection.query("set character set utf8")
+
+# DBに問い合わせ
+rs = connection.query("SELECT * FROM users")
+
+# 取得データを保存
+connection.query("INSERT INTO テーブル名 VALUES(値1, 値2, ...)")
+
+#取得データを更新
+connection.query("UPDATE テーブル名 SET カラム名1 = 値1, カラム名2 = 値2, ... WHERE 条件式")
+
+# コネクションを閉じる
+connection.close
